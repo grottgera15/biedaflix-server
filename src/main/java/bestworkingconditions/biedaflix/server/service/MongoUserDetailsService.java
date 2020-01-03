32695d3572c86b12f80a443c4ae5661dab9faf90
Aleksander Szamałek <aleksander.szamalek@gmail.com>
2020-01-03 17:35:57 +0100

    MongoUserDetailsService

--- src/main/java/bestworkingconditions/biedaflix/server/service/MongoUserDetailsService.java
+++ src/main/java/bestworkingconditions/biedaflix/server/service/MongoUserDetailsService.java
@@ -1,17 +1,28 @@
 package bestworkingconditions.biedaflix.server.service;
 
+import bestworkingconditions.biedaflix.server.model.User;
+import bestworkingconditions.biedaflix.server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 @Profile("mongo")
 public class MongoUserDetailsService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public MongoUserDetailsService(UserRepository userRepository) {this.userRepository = userRepository;}
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findUserByEmail(username);
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(), new ArrayList<>());
     }
 }
