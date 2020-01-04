b4ace5778ca1346c81cac0eb4bd0705a29a9fdef
Aleksander Szamałek <aleksander.szamalek@gmail.com>
2020-01-04 19:12:56 +0100

    getAllSeries endpoint

--- src/main/java/bestworkingconditions/biedaflix/server/controller/SeriesController.java
+++ src/main/java/bestworkingconditions/biedaflix/server/controller/SeriesController.java
@@ -4,11 +4,13 @@ import bestworkingconditions.biedaflix.server.model.Series;
 import bestworkingconditions.biedaflix.server.repository.SeriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class SeriesController {
@@ -17,6 +19,13 @@ public class SeriesController {
 
     @Autowired
     public SeriesController(SeriesRepository repository) {this.repository = repository;}
+
+    @GetMapping("/series")
+    public ResponseEntity<List<Series>> GetAll(){
+        List<Series> availableSeries = repository.findAll();
+        return ResponseEntity.ok(availableSeries);
+    }
+
 
     @PostMapping("/series")
     public ResponseEntity<Series> AddSeries(@Valid @RequestBody Series series){
--- src/main/java/bestworkingconditions/biedaflix/server/model/Season.java
+++ src/main/java/bestworkingconditions/biedaflix/server/model/Season.java
@@ -1,11 +1,32 @@
 package bestworkingconditions.biedaflix.server.model;
 
-import org.springframework.data.mongodb.core.mapping.DBRef;
-
 import java.util.List;
 
 public class Season {
     private int seasonNumber;
-    @DBRef
     private List<Episode> episodes;
+
+    public Season() {
+    }
+
+    public Season(int seasonNumber, List<Episode> episodes) {
+        this.seasonNumber = seasonNumber;
+        this.episodes = episodes;
+    }
+
+    public int getSeasonNumber() {
+        return seasonNumber;
+    }
+
+    public void setSeasonNumber(int seasonNumber) {
+        this.seasonNumber = seasonNumber;
+    }
+
+    public List<Episode> getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(List<Episode> episodes) {
+        this.episodes = episodes;
+    }
 }
