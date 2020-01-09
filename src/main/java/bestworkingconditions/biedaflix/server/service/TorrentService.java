d9636dea1a503522aea272da5eaf5f5b8141cecb
Boladar <aleksander.szamalek@gmail.com>
2020-01-09 16:57:20 +0100

    Torrent service first test

--- src/main/java/bestworkingconditions/biedaflix/server/service/TorrentService.java
+++ src/main/java/bestworkingconditions/biedaflix/server/service/TorrentService.java
@@ -1,6 +1,5 @@
 package bestworkingconditions.biedaflix.server.service;
 
 public interface TorrentService {
-
-
+    public void addTorrent(String magnet);
 }
---
+++ src/main/java/bestworkingconditions/biedaflix/server/service/TorrentServiceImpl.java
@@ -0,0 +1,31 @@
+package bestworkingconditions.biedaflix.server.service;
+
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class TorrentServiceImpl implements TorrentService {
+
+    @Override
+    public void addTorrent(String magnet) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String,String> map = new LinkedMultiValueMap<>();
+        map.add("urls",magnet);
+
+        HttpEntity<MultiValueMap<String,String>> request = new HttpEntity<>(map,headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.postForEntity("localhost:8080/api/v2/torrents/add",request,String.class);
+
+    }
+}
