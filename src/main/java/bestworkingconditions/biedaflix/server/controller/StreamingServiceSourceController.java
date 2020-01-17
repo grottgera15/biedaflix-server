9fb3ff4b4545e62ecd8fa1c91665816ea1c977bf
Boladar <aleksander.szamalek@gmail.com>
2020-01-17 15:10:50 +0100

    added option to get all available StreamingServiceSources

--- src/main/java/bestworkingconditions/biedaflix/server/controller/StreamingServiceSourceController.java
+++ src/main/java/bestworkingconditions/biedaflix/server/controller/StreamingServiceSourceController.java
@@ -12,9 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -31,6 +32,11 @@ public class StreamingServiceSourceController {
     @PostMapping(value = "/streamingSource", consumes = {"multipart/form-data"})
     public ResponseEntity<?> addStreamingServiceSource(@RequestParam(name="name") String name, @RequestParam(name="logo")MultipartFile logo) throws IOException {
 
+        List<StreamingServiceSource> streamingServiceSources = repository.findAll();
+
+        if(streamingServiceSources.stream().anyMatch(t-> t.getName().equals(name)))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"streamingSource of given name already exists in the database!");
+
         StreamingServiceSource newSource = new StreamingServiceSource();
         newSource.setName(name);
         newSource.setMimeType(logo.getContentType());
@@ -44,7 +50,13 @@ public class StreamingServiceSourceController {
     @GetMapping(value = "/streamingSource")
     public ResponseEntity<?> getListOfAllStreamingServiceSources(){
         List<StreamingServiceSource> streamingServiceSources = repository.findAll();
-        throw new NotImplementedException();
+        List<StreamingServiceSourceResponse> response = new ArrayList<>();
+
+        for(StreamingServiceSource source : streamingServiceSources){
+            response.add(new StreamingServiceSourceResponse(source.getId(),source.getName(),source.getResourceURI()));
+        }
+
+        return ResponseEntity.ok(response);
     }
 
 }
--- src/main/java/bestworkingconditions/biedaflix/server/model/response/StreamingServiceSourceResponse.java
+++ src/main/java/bestworkingconditions/biedaflix/server/model/response/StreamingServiceSourceResponse.java
@@ -1,14 +1,40 @@
 package bestworkingconditions.biedaflix.server.model.response;
 
 import java.io.Serializable;
+import java.net.URI;
 
-public class StreamingServiceSourceResponse implements Serializable {
+public class StreamingServiceSourceResponse implements  Serializable{
     private String id;
     private String name;
+    private URI resourcePath;
 
-    public StreamingServiceSourceResponse(String id, String name) {
+    public StreamingServiceSourceResponse(String id, String name,URI resourcePath) {
         this.id = id;
+        this.name = name;
+        this.resourcePath = resourcePath;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
+    public URI getResourcePath() {
+        return resourcePath;
+    }
+
+    public void setResourcePath(URI resourcePath) {
+        this.resourcePath = resourcePath;
+    }
 }
