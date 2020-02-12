3c3467b2f604cf50ad012d0a1de835b132562519
Boladar <aleksander.szamalek@gmail.com>
2020-02-12 14:57:51 +0100

    added cors config

--- src/main/java/bestworkingconditions/biedaflix/server/properties/AppProperties.java
+++ src/main/java/bestworkingconditions/biedaflix/server/properties/AppProperties.java
@@ -5,6 +5,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "biedaflix.config")
 public class AppProperties {
     private String apiDomain;
+    private String domain;
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
 
     public String getApiDomain() {
         return apiDomain;
