865fd3869c1232d90ec3417e683453b7ae6bd5c7
Boladar <aleksander.szamalek@gmail.com>
2020-03-02 17:18:19 +0100

    updated MongoUserDetailsService

--- src/main/java/bestworkingconditions/biedaflix/server/service/MongoUserDetailsService.java
+++ src/main/java/bestworkingconditions/biedaflix/server/service/MongoUserDetailsService.java
@@ -1,15 +1,19 @@
 package bestworkingconditions.biedaflix.server.service;
 
 import bestworkingconditions.biedaflix.server.model.User;
+import bestworkingconditions.biedaflix.server.model.authority.Operation;
+import bestworkingconditions.biedaflix.server.model.authority.Role;
 import bestworkingconditions.biedaflix.server.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Profile("mongo")
@@ -23,7 +27,15 @@ public class MongoUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findUserByEmail(username);
-        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(), new ArrayList<>());
+
+        List<GrantedAuthority> grantedAuthorityList = new ArrayList<>();
+
+        for ( Role role : user.getRoles() ){
+            grantedAuthorityList.add(role);
+            grantedAuthorityList.addAll(role.getAllowedOperations());
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(), grantedAuthorityList);
     }
 
 
