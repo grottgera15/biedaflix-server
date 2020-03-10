194899ed66de5cf0b90d67b2bce01235fb17fc4a
Boladar <aleksander.szamalek@gmail.com>
2020-03-10 16:09:59 +0100

    removed empty tests

--- src/test/java/bestworkingconditions/biedaflix/server/ServerApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package bestworkingconditions.biedaflix.server;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ServerApplicationTests {
-
-}
