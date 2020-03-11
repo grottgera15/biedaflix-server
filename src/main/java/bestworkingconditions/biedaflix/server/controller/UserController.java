16c832b99e50c8a0e547c758109081c2be0b3464
Boladar <aleksander.szamalek@gmail.com>
2020-03-11 16:46:08 +0100

    moved admin endpoints to separate controller

--- src/main/java/bestworkingconditions/biedaflix/server/controller/UserController.java
+++ src/main/java/bestworkingconditions/biedaflix/server/controller/UserController.java
@@ -1,14 +1,11 @@
 package bestworkingconditions.biedaflix.server.controller;
 
 import bestworkingconditions.biedaflix.server.model.User;
-import bestworkingconditions.biedaflix.server.model.request.UserAdministrateRequest;
 import bestworkingconditions.biedaflix.server.model.request.UserRegisterRequest;
-import bestworkingconditions.biedaflix.server.model.response.UserAdministrateResponse;
 import bestworkingconditions.biedaflix.server.model.response.UserResponse;
 import bestworkingconditions.biedaflix.server.repository.UserRepository;
 import bestworkingconditions.biedaflix.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,7 +15,6 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,65 +31,6 @@ public class UserController {
         this.repository = repository;
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
-    }
-
-    @PutMapping(value = "/admin/users/{id}")
-    @PreAuthorize("hasAuthority('OP_ADMINISTRATE_USERS')")
-    public ResponseEntity<?> updateUser(@Valid @RequestBody UserAdministrateRequest administrateRequest, @PathVariable String id){
-
-        Optional<User> match = repository.findById(id);
-
-        if( match.isPresent() && match.get().getId().equals(id)){
-
-            User u = match.get();
-            u.setRoles(administrateRequest.getRoles());
-            u.setAccepted(administrateRequest.isAccepted());
-
-            u = repository.save(u);
-            return ResponseEntity.ok(userService.CreateUserAdministrateResponseFromUser(u));
-        }else{
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"username or email already taken!");
-        }
-    }
-
-    @DeleteMapping(value = "/admin/users/{id}")
-    @PreAuthorize("hasAuthority('OP_ADMINISTRATE_USERS')")
-    public ResponseEntity<?> deleteUser(
-            @PathVariable String id){
-        repository.deleteById(id);
-
-        return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping(value= "/admin/users/{id}")
-    @PreAuthorize("hasAuthority('OP_ADMINISTRATE_USERS')")
-    public ResponseEntity<?> GetSingleAdministrativeUser(
-            @PathVariable String id
-    ){
-        return ResponseEntity.ok(userService.CreateUserAdministrateResponseFromUser(repository.findById(id).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"user of given id does not exist!")
-        )));
-    }
-
-    @GetMapping(value = "/admin/users")
-    @PreAuthorize("hasAuthority('OP_ADMINISTRATE_USERS')")
-    public ResponseEntity<?> getAllUsers(
-            @RequestParam(required = false) Optional<String> roleId,
-            @RequestParam(required = false) Optional<Boolean> accepted
-    ){
-
-        User example = new User();
-        example.setAccepted(null);
-        example.setRoles(null);
-
-        roleId.ifPresent(s -> example.setRoles(Collections.singletonList(s)));
-        accepted.ifPresent(example::setAccepted);
-
-        List<User> requestedUsers = repository.findAll(Example.of(example));
-        List<UserAdministrateResponse> userAdministrateResponses = new ArrayList<>();
-
-        requestedUsers.forEach( x -> userAdministrateResponses.add(userService.CreateUserAdministrateResponseFromUser(x)));
-        return ResponseEntity.ok(userAdministrateResponses);
     }
 
     @PatchMapping(value = "/users/{id}" , consumes = {"multipart/form-data","application/json"})
