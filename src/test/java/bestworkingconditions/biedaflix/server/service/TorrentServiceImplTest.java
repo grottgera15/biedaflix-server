b4847f648cbefd88bc1d6fe972721941c3488dc0
Jakub Adamski <kuba@exemplum.pl>
2020-03-21 21:32:56 +0100

    parseTorrentTest final

--- src/test/java/bestworkingconditions/biedaflix/server/service/TorrentServiceImplTest.java
+++ src/test/java/bestworkingconditions/biedaflix/server/service/TorrentServiceImplTest.java
@@ -5,13 +5,13 @@ import bestworkingconditions.biedaflix.server.model.Episode;
 import bestworkingconditions.biedaflix.server.model.Series;
 import bestworkingconditions.biedaflix.server.model.TorrentFileInfo;
 import bestworkingconditions.biedaflix.server.model.TorrentInfo;
+import bestworkingconditions.biedaflix.server.model.EpisodeVideo.VideoQuality;
 import bestworkingconditions.biedaflix.server.repository.CurrentlyDownloadingRepository;
 import bestworkingconditions.biedaflix.server.repository.EpisodeRepository;
 import bestworkingconditions.biedaflix.server.repository.SeriesRepository;
 import bestworkingconditions.biedaflix.server.repository.TorrentUriRepository;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -19,9 +19,14 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.content.fs.io.FileSystemResourceLoader;
 import org.springframework.web.client.RestTemplate;
 
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.doReturn;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -52,16 +57,28 @@ class TorrentServiceImplTest {
     @Mock
     TorrentInfo torrentInfo;
 
+    FileSystemResourceLoader fileSystemResourceLoader = new FileSystemResourceLoader("files/");
+    File filesystemRoot = new File(System.getProperty("user.dir") + "/files");
+
     @BeforeEach
     public void init() {
         MockitoAnnotations.initMocks(this);
-        FileSystemResourceLoader fileSystemResourceLoader = new FileSystemResourceLoader("files/");
-        File filesystemRoot = new File(System.getProperty("user.dir") + "/files");
         torrentService = new TorrentServiceImpl(torrentUriRepository, fileSystemResourceLoader, currentlyDownloadingRepository, seriesRepository, filesystemRoot, episodeRepository, episodeService, restTemplate);
     }
 
     @Test
-    void parseFinishedTorrentsExecutorTest() throws Exception {
+    void parseFinishedTorrentsExecutorTest() {
+
+        //musze przekopiowac plik mkv
+        Path original = Paths.get(System.getProperty("user.dir") + "/torrentTest.mkv");
+        Path copied = Paths.get(System.getProperty("user.dir") + "/downloads/biedaflix_finished/torrentTest.mkv");
+        try {
+            Files.copy(original, copied, StandardCopyOption.REPLACE_EXISTING);
+        }
+        catch(Exception e){
+            fail("Failed to copy test files");
+        }
+
         //tworze obiekt currentlyDownloading
         CurrentlyDownloading currentlyDownloading = new CurrentlyDownloading();
         //currentlyDownloading ma informacje o torrencie
@@ -80,8 +97,8 @@ class TorrentServiceImplTest {
         doReturn(seriesOptional).when(seriesRepository).findById(seriesId);
         //potem wywołuje normalizeRequestedFiles, które wywołuje renameTorrentFileInfos i tutaj potrzebuje torrentFileInfo
         //torentFileInfo odnosi się do pliku mkv który stworzyłem!
-        String relativePath = "test/lol.mkv";
-        Long fileSize = 2712067l;
+        String relativePath = "/torrentTest.mkv";
+        Long fileSize = 2416407l;
         TorrentFileInfo torrentFileInfo = new TorrentFileInfo(relativePath, fileSize);
         List<TorrentFileInfo> torrentFileInfos = new ArrayList<>();
         torrentFileInfos.add(torrentFileInfo);
@@ -90,19 +107,65 @@ class TorrentServiceImplTest {
         doReturn(currentlyDownloading).when(currentlyDownloadingRepository).save(currentlyDownloading);
         //potem bierze najwiekszy plik z folderu, nie powinno sie wyjebac
         //w tym momencie powinno się wywołać konwertowanie
-        //teraz powinno się wyjebać na deleteTorrent
 
         List<CurrentlyDownloading> currentlyDownloadingList = new ArrayList<>();
         currentlyDownloadingList.add(currentlyDownloading);
         doReturn(currentlyDownloadingList).when(currentlyDownloadingRepository).findAll();
-
         //mozna to pozniej jakos ladnie robic zeby testowal funkcje scheduled
-        //pytanie czy torrentService sie nie wypierdoli bo sa w niej te scheduled funkcje
-        
-        torrentService.parseFinishedTorrentsExecutor();
+
+        try{
+            torrentService.parseFinishedTorrentsExecutor();
+            //sprawdzanie czy utworzyl dobre pliki vid
+            for(VideoQuality q : VideoQuality.values()){
+                File vid = new File(System.getProperty("user.dir") + "/files/series/"+ seriesId + "/" + episode.getId() + "/" + q.getQuality() + ".mp4");
+                if (!vid.exists()){
+                    throw new Exception(q.getQuality() + " file not created");
+                }
+            }
+            //czy jest output
+            File out = new File(System.getProperty("user.dir") + "/files/series/"+ seriesId + "/" + episode.getId() + "/output.txt");
+            if (!out.exists()){
+                throw new Exception("output.txt file not created");
+            }
+            //sprawdzanie czy cos jest w thumbs
+            File thumbs = new File(System.getProperty("user.dir") + "/files/series/"+ seriesId + "/" + episode.getId() + "/thumbs");
+            if (thumbs.isDirectory()){
+                if (thumbs.length() == 0){
+                    throw new Exception("thumbs dir is empty");
+                }
+            }
+            else {
+                throw new Exception("there is no thumbs dir");
+            }
+            //sprawdzanie czy oryginalny plik został usunięty
+            File originalMKV = new File(System.getProperty("user.dir") + "/downloads/biedafilix_finished/torrentTest.mkv");
+            if (originalMKV.exists()){
+                throw new Exception("original mkv was not deleted");
+            }
+            //czyszczenie folderow
+            File seriesFolder = new File(System.getProperty("user.dir") + "/files/series/"+ seriesId);
+            deleteFile(seriesFolder);
+        }
+        catch(Exception e){
+            fail("Error " + e);
+        }
     }
 
-    /*
+    //usuwanie plikow
+    void deleteFile(File file){
+        if (file.isDirectory()){
+            String[] entries = file.list();
+            for(String s : entries){
+                File currentFile = new File(file.getPath(), s);
+                deleteFile(currentFile);
+            }
+            file.delete();
+        }
+        else{
+            file.delete();
+        }
+    }
+
     @Test
     void renameTorrentFileInfosTest_withMultipleFiles() {
 
@@ -142,5 +205,5 @@ class TorrentServiceImplTest {
         File parentFile = new File(System.getProperty("user.dir") + "/downloads/biedaflix_finished/" + "The_Mandalorian_S01E02_1080p_WEBRip_x264_AAC_5.1_ESubs_-_LOKiHD_-_Telly/");
 
         Assert.assertEquals(parentFile, TorrentServiceImpl.getCurrentlyDownloadingDirectory(currentlyDownloading));
-    } */
+    }
 }
