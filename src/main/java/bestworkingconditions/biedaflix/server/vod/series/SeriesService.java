de9228b87b9e9eb0c5a934168a75ba67027fc977
Aleksander Szamałek <aleksander.szamalek@gmail.com>
2020-03-21 02:52:49 +0100

    moved SeriesService and StreamingServiceSourceService to new GenericFileHandlinhServiceImpl

--- src/main/java/bestworkingconditions/biedaflix/server/vod/series/SeriesService.java
+++ src/main/java/bestworkingconditions/biedaflix/server/vod/series/SeriesService.java
@@ -1,27 +1,22 @@
 package bestworkingconditions.biedaflix.server.vod.series;
 
-import bestworkingconditions.biedaflix.server.common.service.GenericServiceImpl;
 import bestworkingconditions.biedaflix.server.file.service.FileService;
+import bestworkingconditions.biedaflix.server.file.service.GenericFileHandlingServiceImpl;
 import bestworkingconditions.biedaflix.server.vod.series.model.Series;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class SeriesService extends GenericServiceImpl<Series,SeriesRepository> {
-
-    private final FileService fileService;
+public class SeriesService extends GenericFileHandlingServiceImpl<Series,SeriesRepository> {
 
     @Autowired
     public SeriesService(SeriesRepository repository, FileService fileService) {
-        super(repository);
-        this.fileService = fileService;
+        super(repository, fileService);
     }
 
-    public Series AddLogo(String id,MultipartFile logo){
-        Series series = findById(id);
-        series.setLogo(fileService.saveFile(logo));
-        return repository.save(series);
+    public Series AddLogo(String id, MultipartFile logo){
+        return setFileReference(id,logo,Series::setLogo);
     }
 
 }
--- src/main/java/bestworkingconditions/biedaflix/server/vod/streamingServiceSource/StreamingServiceSourceService.java
+++ src/main/java/bestworkingconditions/biedaflix/server/vod/streamingServiceSource/StreamingServiceSourceService.java
@@ -1,26 +1,21 @@
 package bestworkingconditions.biedaflix.server.vod.streamingServiceSource;
 
-import bestworkingconditions.biedaflix.server.common.service.GenericServiceImpl;
 import bestworkingconditions.biedaflix.server.file.service.FileService;
+import bestworkingconditions.biedaflix.server.file.service.GenericFileHandlingServiceImpl;
 import bestworkingconditions.biedaflix.server.vod.streamingServiceSource.model.StreamingServiceSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class StreamingServiceSourceService extends GenericServiceImpl<StreamingServiceSource,StreamingServiceSourceRepository> {
-
-    private final FileService fileService;
+public class StreamingServiceSourceService extends GenericFileHandlingServiceImpl<StreamingServiceSource,StreamingServiceSourceRepository> {
 
     @Autowired
     public StreamingServiceSourceService(StreamingServiceSourceRepository repository, FileService fileService) {
-        super(repository);
-        this.fileService = fileService;
+        super(repository, fileService);
     }
 
     public StreamingServiceSource setLogo(String id, MultipartFile file){
-        StreamingServiceSource request = findById(id);
-        request.setLogo(fileService.saveFile(file));
-        return repository.save(request);
+        return setFileReference(id,file,StreamingServiceSource::setLogo);
     }
 }
